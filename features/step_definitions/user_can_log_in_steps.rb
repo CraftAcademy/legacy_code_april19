83f0f56fd3e127c40519f1722e740581f62d5c9d
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-28 17:56:24 +0200

    Removes duplicate steps between previous development.

--- features/step_definitions/user_can_log_in_steps.rb
+++ features/step_definitions/user_can_log_in_steps.rb
@@ -1,22 +1,5 @@
-Given("the following user exists") do |table|
-    table.hashes.each do |user|
-        FactoryBot.create(:user, user)
-    end
-end 
-
-When("I visit the landing page") do
-    visit root_path
-end
-
-And("I click {string} link") do |string|
-    click_link string 
-end
-
 When("I fill in the {string} with {string}") do |field, user_data|
     fill_in field, with: user_data
 end
 
-When("I click {string} button") do |login|
-    click_button login
-end
 
